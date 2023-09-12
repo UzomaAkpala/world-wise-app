b78c37231013f8692da8e3b4efcb8d5461de9d00
UzomaAkpala <francisakpala@yahoo.com>
2023-09-12 13:11:59 +0100

    Using css modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Product from "./pages/Product";
 import Prices from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Prices />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Prices />} />
+        <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
